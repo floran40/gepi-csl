eca686759d2188a6c072f4b3ba3c43e3e06fc966
floran40 <floran40250@gmail.com>
2020-03-24 22:15:46 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php
 /*
  *
- * Copyright 2009-2019 Josselin Jacquard, Stephane Boireau
+ * Copyright 2001-2011 Thomas Belliard, Laurent Delineau, Edouard Hue, Eric Lebrun
  *
  * This file is part of GEPI.
  *
  * GEPI is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or
+ * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
  *
  * GEPI is distributed in the hope that it will be useful,
@@ -20,266 +20,236 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-$filtrage_extensions_fichiers_table_ct_types_documents='y';
+/**
+ * Gestion des cahiers de textes
+ * 
+ * @param $_POST['activer'] activation/désactivation
+ * @param $_POST['version'] numero de version du cahier de texte
+ * @param $_POST['cahiers_texte_login_pub'] identifiant accès public
+ * @param $_POST['cahiers_texte_passwd_pub'] mot de passe accès public
+ * @param $_POST['begin_month'],$_POST['begin_day'],$_POST['begin_year'] Date de début du cahier de texte
+ * @param $_POST['end_month'],$_POST['end_day'],$_POST['end_year'] Date de fin du cahier de texte
+ * @param $_POST['cahier_texte_acces_public'] Accès public du cahier de texte
+ * @param $_POST['visa_cdt_inter_modif_notices_visees'] Interdiction de modifier les notices visées
+ * @param $_POST['delai_devoirs'] Délai de visualisation des devoirs
+ * @param $_POST['is_posted']
+ *
+ */
 
-// On désamorce une tentative de contournement du traitement anti-injection lorsque register_globals=on
-if (isset($_GET['traite_anti_inject']) || isset($_POST['traite_anti_inject'])) $traite_anti_inject = "yes";
+$accessibilite="y";
+$titre_page = "Gestion des cahiers de textes";
+$niveau_arbo = 1;
+$gepiPathJava="./..";
 
 // Initialisations files
-include("../lib/initialisationsPropel.inc.php");
 require_once("../lib/initialisations.inc.php");
-
-//=================================
-//
-//	emplois du temps - requirements
-//
-//=================================
-include("../edt_organisation/cdt_requirements.php");
-
-
-
-//echo("Debug Locale : ".setLocale(LC_TIME,0));
 
 // Resume session
 $resultat_session = $session_gepi->security_check();
 if ($resultat_session == 'c') {
-    header("Location: ../utilisateurs/mon_compte.php?change_mdp=yes");
-    die();
+   header("Location: ../utilisateurs/mon_compte.php?change_mdp=yes");
+   die();
 } else if ($resultat_session == '0') {
-    header("Location: ../logout.php?auto=1");
-    die();
+   header("Location: ../logout.php?auto=1");
+   die();
 }
 
+// Check access
 if (!checkAccess()) {
-    header("Location: ../logout.php?auto=1");
-    die();
+   header("Location: ../logout.php?auto=1");
+   die();
 }
 
-//On vérifie si le module est activé
-//if (getSettingValue("active_cahiers_texte")!='y') {
-if(!acces_cdt()) {
-    die("Le module n'est pas activé.");
-}
-
-//recherche de l'utilisateur avec propel
-$utilisateur = UtilisateurProfessionnelPeer::getUtilisateursSessionEnCours();
-if ($utilisateur == null) {
-	header("Location: ../logout.php?auto=1");
-	die();
-}
-
-// On met le header en petit par défaut
-$_SESSION['cacher_header'] = "y";
-//**************** EN-TETE *****************
-$titre_page = "Cahier de textes";
-
-$style_specifique[] = "lib/DHTMLcalendar/calendarstyle";
-$javascript_specifique[] = "cahier_texte_2/init_cahier_texte_2";
-$utilisation_win = 'oui';
-//$utilisation_jsdivdrag = "non";
-$windows_effects = "non";
-$message_deconnexion = "non";
-
-//on regarde si les preferences pour le cdt sont precisees dans la requete
-$cdt_version_pref = isset($_POST["cdt_version_pref"]) ? $_POST["cdt_version_pref"] :(isset($_GET["cdt_version_pref"]) ? $_GET["cdt_version_pref"] :NULL);
-if ($cdt_version_pref != null) {
-    $utilisateur->setPreferenceValeur("cdt_version", $cdt_version_pref);
-}
-
-//on regarde les preference de l'utilisateur
-if ($utilisateur->getPreferenceValeur("cdt_version") == "1") {
-    header("Location: ../cahier_texte/index.php?cdt_version_pref=1");
-    die();
-}
-
-
-//on reste sur le cdt1, le navigateur n'etant pas compatible avec le cdt2
-if (strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE 6' ) !== FALSE) {
-    header("Location: ../cahier_texte/index.php");
-    die();
-}
-//=================================
-//
-//		Init emplois du temps
-//
-//=================================
-$edt_liens_target_blank="y";
-include("../edt_organisation/cdt_initialisation.php");
-$pas_de_message_deconnexion = 1;
-
-require_once("../lib/header.inc.php");
-//**************** FIN EN-TETE *************
-//-----------------------------------------------------------------------------------
-
-//debug_var();
-
-insere_lien_calendrier_crob("right");
-
-// si l'id d'un groupe est spécifié, on l'enregistre dans un champ hidden, il sera utilisé par le javascript d'initialisation pour basculer vers le groupe concerné
-echo "<input type='hidden' name='id_groupe_init' id='id_groupe_init' value='";
-$id_groupe = isset($_POST["id_groupe"]) ? $_POST["id_groupe"] :(isset($_GET["id_groupe"]) ? $_GET["id_groupe"] :NULL);
-if ($id_groupe != NULL) {
-	echo $_GET["id_groupe"];
-} else if (isset($_SESSION['id_groupe_session'])) {
-	echo $_SESSION['id_groupe_session'];
-}
-echo "' />\n";
-
-//============================================
-// Pour pouvoir pointer une notice précise depuis une page externe:
-echo "<input type='hidden' name='type_notice_init' id='type_notice_init' value='";
-$type_notice = isset($_POST["type_notice"]) ? $_POST["type_notice"] :(isset($_GET["type_notice"]) ? $_GET["type_notice"] :NULL);
-if ($type_notice != NULL) {
-	echo $type_notice;
-}
-echo "' />\n";
-//echo "\$type_notice=$type_notice<br />";
-
-echo "<input type='hidden' name='id_ct_init' id='id_ct_init' value='";
-$id_ct = isset($_POST["id_ct"]) ? $_POST["id_ct"] :(isset($_GET["id_ct"]) ? $_GET["id_ct"] :NULL);
-if ($id_ct != NULL) {
-	echo $id_ct;
-}
-echo "' />\n";
-//echo "\$id_ct=$id_ct<br />";
-//============================================
-
-echo "<table width=\"98%\" cellspacing=0 align=\"center\" summary=\"Tableau d'entète\">\n";
-echo "<tr>\n";
-echo "<td valign='center'>\n";
-echo "<button style='width: 200px;' onclick=\"javascript:
-						getWinDernieresNotices().show();
-						getWinDernieresNotices().toFront();
-						return false;
-				\">Voir les dernières notices</button>\n";
-echo "<br />";
-// Comment tester qu'une fenêtre est actuellement affichée?
-echo "<button style='width: 200px;' onclick=\"javascript:
-						getWinDernieresNotices().setLocation(155, 40);
-						getWinDernieresNotices().hide();
-						getWinCalendar().setLocation(0, GetWidth() - 245);
-						getWinEditionNotice().setLocation(160, 334);
-						getWinEditionNotice().setSize(GetWidth()-360, GetHeight() - 160);
-						getWinListeNotices().setLocation(160, 0);
-						getWinListeNotices().setSize(330, GetHeight() - 160);
-						if(document.getElementById('win_banque_texte')) {
-							if(document.getElementById('win_banque_texte').style.display!='none') {
-								getWinBanqueTexte().setLocation(10, 40);
-							}
-						}
-						if(document.getElementById('win_car_spec')) {
-							if(document.getElementById('win_car_spec').style.display!='none') {
-								getWinCarSpec().setLocation(10, 340);
-							}
-						}
-						if(document.getElementById('win_archives')) {
-							if(document.getElementById('win_archives').style.display!='none') {
-								getWinArchives().setLocation(100, 400);
-							}
-						}
-						if(document.getElementById('win_liste_notices_privees')) {
-							if(document.getElementById('win_liste_notices_privees').style.display!='none') {
-								getWinListeNoticesPrivees().setLocation(0, 100);
-							}
-						}
-						if(document.getElementById('win_dev_classe')) {
-							if(document.getElementById('win_dev_classe').style.display!='none') {
-								getWinDevoirsDeLaClasse().setLocation(0, 304);
-							}
-						}
-						return false;
-				\">Repositionner les fenetres</button>\n";
-echo "</td>";
-
-echo "<td width='20 px'>";
-echo "<a href=\"./index.php?cdt_version_pref=1\">\n";
-echo "<img src='../images/icons/cdt2_1.png' alt='Utiliser la version 1 du cahier de textes' class='link' title='Utiliser la version 1 du cahier de textes'/> </a>";
-//echo "<button style='width: 200px;' onclick=\"javascript:window.location.replace('./index.php?cdt_version_pref=1')
-//				\">Utiliser la version 1 du cahier de textes</button>\n";
-echo "</td>";
-// **********************************************
-// Affichage des différents groupes du professeur
-// Récupération de toutes les infos sur le groupe
-echo "<td valign='center'>";
-$groups = $utilisateur->getGroupes();
-if ($groups->isEmpty()) {
-    echo "<br /><br />";
-    echo "<b>Aucun cahier de textes n'est disponible.</b>";
-    echo "<br /><br />";
-}
-
-if(!getSettingAOui("active_cahiers_texte")) {
-	// On doit avoir acces_cdt_prof=y
-	echo "<p style='text-align:center; color:red'>Ces cahiers de textes sont personnels. Ils ne sont pas accessibles des élèves, parents,...";
-	if(getSettingValue("acces_cdt_prof_url_cdt_officiel")!="") {
-		echo "<br />Les CDT officiels <em>(consultés par les élèves,...)</em> sont à l'adresse <a href='".getSettingValue("acces_cdt_prof_url_cdt_officiel")."' target='_blank'>".getSettingValue("acces_cdt_prof_url_cdt_officiel")."</a>";
+/******************************************************************
+ *    Enregistrement des variables passées en $_POST si besoin
+ ******************************************************************/
+$msg = "";
+if (isset($_POST['is_posted'])) {
+	check_token();
+	//debug_var();
+	if (isset($_POST['activer'])) {
+		if (!saveSetting("active_cahiers_texte", $_POST['activer'])) $msg = "Erreur lors de l'enregistrement du paramètre activation/désactivation !<br />";
 	}
-	echo "</p>";
-}
 
-$nom_ou_description_groupe_cdt=getPref($_SESSION['login'], "nom_ou_description_groupe_cdt", "name");
-//echo "\$nom_ou_description_groupe_cdt=$nom_ou_description_groupe_cdt<br />";
+	if (isset($_POST['acces_archives_cdt'])) {
+		if (!saveSetting("acces_archives_cdt", "y")) $msg = "Erreur lors de l'enregistrement du paramètre d'accès aux Archives CDT !<br />";
+	}
+	else {
+		if (!saveSetting("acces_archives_cdt", "n")) $msg = "Erreur lors de l'enregistrement du paramètre d'accès aux Archives CDT !<br />";
+	}
 
-$a = 1;
-foreach($groups as $group) {
-	$sql="SELECT 1=1 FROM j_groupes_visibilite WHERE id_groupe='".$group->getId()."' AND domaine='cahier_texte' AND visible='n';";
-	$test_grp_visib=mysqli_query($GLOBALS["mysqli"], $sql);
-	if(mysqli_num_rows($test_grp_visib)==0) {
-		echo "<a href=\"#\" style=\"font-size: 11pt;\"  onclick=\"javascript:
-				id_groupe = '".$group->getId()."';
-				getWinDernieresNotices().hide();
-				getWinListeNotices();
-				new Ajax.Updater('affichage_liste_notice', './ajax_affichages_liste_notices.php?id_groupe=".$group->getId()."', {encoding: 'utf-8'});
-				getWinEditionNotice().setAjaxContent('./ajax_edition_compte_rendu.php?id_groupe=".$group->getId()."&today='+getCalendarUnixDate(), { 
-				    		encoding: 'utf-8',
-				    		onComplete : 
-				    		function() {
-				    			initWysiwyg();
-							}
-						}
-				);
-				return false;
-			\">";
+	if (isset($_POST['acces_cdt_prof'])) {
+		if (!saveSetting("acces_cdt_prof", "y")) $msg = "Erreur lors de l'enregistrement du paramètre acces_cdt_prof !<br />";
+	}
+	else {
+		if (!saveSetting("acces_cdt_prof", "n")) $msg = "Erreur lors de l'enregistrement du paramètre acces_cdt_prof !<br />";
+	}
 
-			echo "<span title=\"".$group->getName()." - ".$group->getDescriptionAvecClasses()." (";
-			$cpt_prof=0;
-			foreach($group->getProfesseurs() as $prof) {
-				if($cpt_prof>0) {echo ", ";}
-				echo casse_mot($prof->getNom(),"maj")." ".casse_mot($prof->getPrenom(),"majf2");
-				$cpt_prof++;
-			}
-			echo ").\">";
-			if($nom_ou_description_groupe_cdt=='name') {
-				echo $group->getNameAvecClasses();
-			}
-			else {
-				echo $group->getDescriptionAvecClasses();
-			}
-			echo "</span>";
-		echo "</a>&nbsp;\n";
+	if (isset($_POST['acces_cdt_prof_afficher_lien'])) {
+		if (!saveSetting("acces_cdt_prof_afficher_lien", $_POST['acces_cdt_prof_afficher_lien'])) $msg = "Erreur lors de l'enregistrement du paramètre acces_cdt_prof_afficher_lien !<br />";
+	}
 
-		if ($a == 3) {
-			$a = 1;
+	if (isset($_POST['acces_cdt_prof_url_cdt_officiel'])) {
+		if (!saveSetting("acces_cdt_prof_url_cdt_officiel", $_POST['acces_cdt_prof_url_cdt_officiel'])) $msg = "Erreur lors de l'enregistrement du paramètre acces_cdt_prof_url_cdt_officiel !<br />";
+	}
+
+
+	if (isset($_POST['version'])) {
+		if (!saveSetting("GepiCahierTexteVersion", $_POST['version'])) $msg = "Erreur lors de l'enregistrement du numero de version du cahier de texte !<br />";
+	}
+	
+	if (isset($_POST['cahiers_texte_login_pub'])) {
+		$mdp = $_POST['cahiers_texte_passwd_pub'];
+		$user_ct = $_POST['cahiers_texte_login_pub'];
+	
+		if ((trim($mdp)=='') and (trim($user_ct) !='')) {
+			$_POST['cahiers_texte_login_pub'] = '';
+			$msg .= "Vous devez choisir un mot de passe.<br />";
+		}
+		if ((trim($mdp) !='')and (trim($user_ct) == '')) {
+			$_POST['cahiers_texte_passwd_pub'] = '';
+			$msg .= "Vous devez choisir un identifiant.<br />";
+		}
+	
+		if (!saveSetting("cahiers_texte_passwd_pub", $_POST['cahiers_texte_passwd_pub']))
+				$msg .= "Erreur lors de l'enregistrement du mot de passe !<br />";
+	//    include_once( '../lib/class.htaccess.php' );
+		if (!saveSetting("cahiers_texte_login_pub", $_POST['cahiers_texte_login_pub']))
+				$msg .= "Erreur lors de l'enregistrement du login !<br />";
+	
+	}
+	
+	if (isset($_POST['begin_day']) and isset($_POST['begin_month']) and isset($_POST['begin_year'])) {
+		$begin_bookings = mktime(0,0,0,$_POST['begin_month'],$_POST['begin_day'],$_POST['begin_year']);
+		if (!saveSetting("begin_bookings", $begin_bookings))
+				$msg .= "Erreur lors de l'enregistrement de begin_bookings !<br />";
+	}
+	if (isset($_POST['end_day']) and isset($_POST['end_month']) and isset($_POST['end_year'])) {
+		$end_bookings = mktime(0,0,0,$_POST['end_month'],$_POST['end_day'],$_POST['end_year']);
+		if (!saveSetting("end_bookings", $end_bookings))
+				$msg .= "Erreur lors de l'enregistrement de end_bookings !<br />";
+	}
+	
+	if (isset($_POST['cahier_texte_acces_public'])) {
+		if ($_POST['cahier_texte_acces_public'] == "yes") {
+			$temp = "yes";
 		} else {
-			$a = $a + 1;
+			$temp = "no";
+			}
+		if (!saveSetting("cahier_texte_acces_public", $temp)) {
+			$msg .= "Erreur lors de l'enregistrement de cahier_texte_acces_public !<br />";
+		}
+	}
+	
+	//ajout Eric visa CDT
+	if (isset($_POST['visa_cdt_inter_modif_notices_visees'])) {
+		if ($_POST['visa_cdt_inter_modif_notices_visees'] == "yes") {
+			$temp = "yes";
+		} else {
+			$temp = "no";
+			}
+		if (!saveSetting("visa_cdt_inter_modif_notices_visees", $temp)) {
+			$msg .= "Erreur lors de l'enregistrement de visa_cdt_inter_modif_notices_visees !<br />";
+		}
+	}
+	//Fin ajout Eric
+	
+	if (isset($_POST['delai_devoirs'])) {
+		if (!saveSetting("delai_devoirs", $_POST['delai_devoirs']))
+				$msg .= "Erreur lors de l'enregistrement du délai de visualisation des devoirs<br />";
+	}
+
+
+	if (isset($_POST['is_posted'])) {
+		if (isset($_POST['cdt_possibilite_masquer_pj'])) {
+			if (!saveSetting("cdt_possibilite_masquer_pj", "y")) {
+				$msg .= "Erreur lors de l'enregistrement de l'autorisation de masquer des documents joints.<br />";
+			}
+		}
+		elseif (!saveSetting("cdt_possibilite_masquer_pj", "n")) {
+			$msg .= "Erreur lors de l'enregistrement de l'interdiction de masquer des documents joints.<br />";
+		}
+	}
+
+
+	if (isset($_POST['is_posted'])) {
+		if (isset($_POST['cdt_afficher_volume_docs_joints'])) {
+			if (!saveSetting("cdt_afficher_volume_docs_joints", "y")) {
+				$msg .= "Erreur lors de l'enregistrement de l'affichage du volume des documents joints.<br />";
+			}
+		}
+		elseif (!saveSetting("cdt_afficher_volume_docs_joints", "n")) {
+			$msg .= "Erreur lors de l'enregistrement de l'affichage du volume des documents joints.<br />";
+		}
+	}
+
+
+	if (isset($_POST['is_posted']) && ($msg=="") ) {
+		$msg = "Les modifications ont été enregistrées !<br />";
+		$post_reussi=TRUE;
+	}
+	
+	if (isset($_POST['cdt_autoriser_modif_multiprof'])) {
+		if ($_POST['cdt_autoriser_modif_multiprof'] == "yes") {
+			$temp = "yes";
+		} else {
+			$temp = "no";
+		}
+		if (!saveSetting("cdt_autoriser_modif_multiprof", $temp)) {
+			$msg .= "Erreur lors de l'enregistrement de cdt_autoriser_modif_multiprof !<br />";
 		}
 	}
 }
-echo "<a href='creer_sequence.php'>Pr&eacute;parer une s&eacute;quence enti&egrave;re</a></td>";
-// Fin Affichage des différents groupes du professeur
-// **********************************************
-echo "<td width='250 px'></td>";
-echo "</tr>\n";
-echo "</table>\n<hr />";
-//=================================
-//
-//	emplois du temps - affichage
-//
-//=================================
-$edt_avec_semAB="y";
-require_once("../edt_organisation/cdt_voir_view.php");
 
-echo "<script type='text/javascript'>".js_checkbox_change_style()."</script>";
+// on demande une validation si on quitte sans enregistrer les changements
+$messageEnregistrer="Des informations ont été modifiées. Voulez-vous vraiment quitter sans enregistrer ?";
 
-require("../lib/footer.inc.php");
+/****************************************************************
+                     HAUT DE PAGE
+****************************************************************/
+
+// ====== Inclusion des balises head et du bandeau =====
+include_once("../lib/header_template.inc.php");
+
+/****************************************************************
+			FIN HAUT DE PAGE
+****************************************************************/
+
+if (!suivi_ariane($_SERVER['PHP_SELF'],$titre_page))
+		echo "erreur lors de la création du fil d'ariane";
+
+/****************************************************************
+			CONSTRUCTION DE LA PAGE
+****************************************************************/
+
+
+/****************************************************************
+			BAS DE PAGE
+****************************************************************/
+$tbs_microtime	="";
+$tbs_pmv="";
+require_once ("../lib/footer_template.inc.php");
+
+/****************************************************************
+			On s'assure que le nom du gabarit est bien renseigné
+****************************************************************/
+if ((!isset($_SESSION['rep_gabarits'])) || (empty($_SESSION['rep_gabarits']))) {
+	$_SESSION['rep_gabarits']="origine";
+}
+
+//==================================
+// Décommenter la ligne ci-dessous pour afficher les variables $_GET, $_POST, $_SESSION et $_SERVER pour DEBUG:
+// $affiche_debug=debug_var();
+
+
+$nom_gabarit = '../templates/'.$_SESSION['rep_gabarits'].'/cahier_texte_admin/index_template.php';
+
+$tbs_last_connection=""; // On n'affiche pas les dernières connexions
+include($nom_gabarit);
+
+// ------ on vide les tableaux -----
+unset($menuAffiche);
+
+
+
 ?>
